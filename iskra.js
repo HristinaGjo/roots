2bc53740d8bd7fb4b9391667214dedd69e6c4015
Hristina Gjorgieva <hgjorgieva@gmail.com>
2025-06-06 11:31:58 +0200

    filling of the cells that form the shape

--- iskra.js
+++ iskra.js
@@ -5,6 +5,20 @@ const settings = {
 };
 
 const sketch = () => {
+
+    // Define the pattern (1 = red square, 0 = empty)
+    const pattern = [
+    [0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 1, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 1, 0, 0],
+    [0, 1, 0, 0, 1, 0, 0, 1, 0],
+    [1, 0, 0, 1, 0, 1, 0, 0, 1],
+    [0, 1, 0, 0, 1, 0, 0, 1, 0],
+    [0, 0, 1, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0]
+  ];
+
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
@@ -22,14 +36,39 @@ const sketch = () => {
       for(let j = 0; j < 9; j++){
         x = ix + (w * i);
         y = iy + (h * j);
-        
+
         context.beginPath();
         context.rect(x, y, w, h);
         context.strokeStyle = 'black';
         context.stroke();
+
+        if (pattern[i][j] === 1){
+          context.fillStyle = 'red'
+          context.fillRect(x,y,w,h)
+        }
       }
     }
   };
 };
 
 canvasSketch(sketch, settings);
+
+
+        /*if (i = 4) {
+          context.beginPath();
+          context.fillStyle = 'black'
+          context.fillRect(x,y,w,h);
+          context.stroke; 
+        }*/
+
+            /*const pattern = [
+    [0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 1, 0, 0, 0],
+    [0, 0, 1, 0, 1, 0, 1, 0, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 0, 1, 0, 1, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0]
+  ];*/
